beacca6db4f0d1a4c2f8d6c2a47466c636e9f01d
MrProVans <77919630+MrProVans@users.noreply.github.com>
2024-01-28 01:47:56 +0300

    Initial Commit

---
+++ Workplace/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Workplace
+//
+//  Created by Иван Клочков on 28.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
