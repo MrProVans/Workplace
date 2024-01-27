b202a0b06dcf7d99fda423dc2094fab514f1b584
MrProVans <77919630+MrProVans@users.noreply.github.com>
2024-01-28 01:58:44 +0300

    New easy program

--- Workplace/main.cpp
+++ Workplace/main.cpp
@@ -11,5 +11,18 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!\n";
+    const int ftInMile=5280;
+    const double kmInMile=1.609344;
+    int dMile,dFt,dKm,dM;
+    cout<<"Mile: ";
+    cin>>dMile;
+    cout<<"Funt: ";
+    cin>>dFt;
+    double distMile=dMile+(double)dFt/ftInMile;
+    double distKm=distMile*kmInMile;
+    dKm=(int)distKm;
+    dM=(int)((distKm-dKm)*1000);
+    cout<<"Kilometres: "<<dKm<<endl;
+    cout<<"metres: "<<dM<<endl;
     return 0;
 }
