75a441c83b407d7d5f2b4e9477903c85f65e9986
MrProVans <77919630+MrProVans@users.noreply.github.com>
2024-01-29 18:44:55 +0300

    task 3 completted

--- Workplace/workplace1.cpp
+++ Workplace/workplace1.cpp
@@ -10,48 +10,27 @@
 
 using namespace std;
 
-template <typename T>
-void printPermutations(T arr[], int size) {
-    for (;;) {
-        for (int i = 0; i < size; ++i) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-
-        if (!next_permutation(arr, arr + size)) {
-            break;
-        }
-    }
-}
-
-template <typename T>
-void inputArray(T arr[], int size) {
-    for (int i = 0; i < size; ++i) {
-        cin >> arr[i];
-    }
-}
-
 int main() {
-    int size;
-    cin >> size;
+    // Ввод двух строк
+    string str1, str2;
+    
+    getline(cin, str1);
+    getline(cin, str2);
 
-    string firstElement;
-    cin >> firstElement;
+    if (str1.length() == str2.length()) {
+        sort(str1.begin(), str1.end());
+        sort(str2.begin(), str2.end());
 
-    if (firstElement.find('.') != string::npos) {
-        double* array = new double[size];
-        array[0] = stod(firstElement);
-        inputArray(array + 1, size - 1);
-        printPermutations(array, size);
-        delete[] array;
+        if (str1 == str2) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
     } else {
-        int* array = new int[size];
-        array[0] = stoi(firstElement);
-        inputArray(array + 1, size - 1);
-        printPermutations(array, size);
-        delete[] array;
+        cout << "No" << endl;
     }
 
     return 0;
 }
+
 
